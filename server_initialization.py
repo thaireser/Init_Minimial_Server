105fa9aa2501683768064a210278bb024b7fe08b
thaireser <148774723+thaireser@users.noreply.github.com>
2024-01-18 13:47:19 +0100

    Update server_initialization.py

--- server_initialization.py
+++ server_initialization.py
@@ -2,6 +2,7 @@
 # -*- coding: utf8 -*-
 
 import argparse
+import distro
 import logging
 import os
 import subprocess
@@ -15,37 +16,37 @@ def add_content_to_file(file, content):
         file.write(content + '\n')
 
 def is_path_existing(path):
-    if os.path.exists(path):
-        log(f"\033[92m...ok.\033[0m")
-        return True
-    log(f"...File does not exist.")
-    return False
+    return os.path.exists(path)
 
-def log(output):
+def log(output, color=None):
     if is_silent:
         logging.info(output)
+        return
+    if color is not None:
+        color_code = getattr(Fore, color.upper(), Fore.RESET)
+        print(f"{color_code}{output}{Style.RESET_ALL}")
         return
     print(f"{output}")
 
 def determine_hostname(hostnames_file):
     counter = 1
-    os_release_command = "lsb_release -a | grep Description | sed 's/ /_/g'| awk '{print $2}'"
-    os_release = subprocess.check_output(os_release_command, shell=True, universal_newlines=True).strip()
-    hostname = f"{os_release}_{counter}"
-    if search_term_in_file(hostnames_file, hostname):
-        counter = counter + 1
-        hostname = f"{os_release}_{counter}"
-        search_term_in_file(hostnames_file, hostname)
+    hostname = (f"{distro.linux_distribution()[0]}-{distro.linux_distribution()[1]}-{counter}").replace(" ", "-")
+    while search_term_in_file(hostnames_file, hostname):
+        counter += 1
+        hostname = (f"{distro.linux_distribution()[0]}-{distro.linux_distribution()[1]}-{counter}").replace(" ", "-")
+        #search_term_in_file(hostnames_file, hostname)
+    add_content_to_file(hostnames_file, hostname)
     return hostname
 
 def determine_ip(ip_adresses_file):
-    ip = "192.168.178.1"
-    with open(ip_adresses_file, 'a+') as file:
-        if not file.read(1):
-            add_content_to_file(ip_adresses_file, ip)
+    with open(ip_adresses_file, 'r') as file:
+        #STANDARD_START_IP
+        ip = "192.168.178.1"
         lines = file.readlines()
-        ip = lines[-1]
-        file.close()
+        if not lines:
+            add_content_to_file(ip_adresses_file, ip)
+            lines = file.readlines()
+        ip = lines[-1].strip()
     if is_ping_successful(ip):
         ip = increment_ip(ip)
         add_content_to_file(ip_adresses_file, ip)
@@ -112,7 +113,6 @@ def configure_log():
 
 def parse_arguments():
     parser = argparse.ArgumentParser(description='This script initializes minimal servers for a very specific context. Please read the documentation before use.')
-    # Command-line options
     parser.add_argument('-s', '--silent', action='store_true', help='Silent mode. Uses automatic IP and hostname configuration. Logs will be written to the current directory.')
     parser.add_argument('-a', '--automatic', action='store_true', help='Automatic IP and hostname configuration.')
     return parser.parse_args()
@@ -121,9 +121,9 @@ def run_command(command):
     try:
         result = subprocess.run(command, shell=True, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE,
                                   universal_newlines=True)
-        log(f"\033[92m...ok.\033[0m")
+        log(f"...ok.", "GREEN")
     except subprocess.CalledProcessError as e:
-        log(f"\033[91m...not ok: {e.stderr.strip()}\033[0m")
+        log(f"...not ok: {e.stderr.strip()}", "RED")
 
 def getting_paths(file):
     shared_dir_command = "df -h | grep Desktop | awk '{print $6}'"
@@ -137,31 +137,32 @@ def getting_paths(file):
 
 def get_directory_containing_file(file):
     paths = getting_paths(file)
+    log(f'Searching for file {file}...')
     for path in paths:
-        log(f'Searching for file {path}...')
         if is_path_existing(path):
+            log(f"...ok: {path} found.", "GREEN")
             return path
-    log(f"\033[91m...not ok: NO LIST WAS FOUND. PROCEEDING WITH LUCK.\033[0m")
-    return path[1]
+        log(f"...not found: {path}")
+    log(f"...not ok: NO LIST WAS FOUND. PROCEEDING WITH LUCK." , "RED")
+    log(f"Creating file {paths[1]}..." , "YELLOW")
+    run_command(f"touch {paths[1]}")
+    return paths[1]
 
 def search_term_in_file(file, search_term):
-    try:
-        with open(file, 'r') as file:
-            content = file.read()
-            if search_term in content:
-                return True
-            return False
-    except Exception as e:
-        print(f"Error checking for term in file: {e}")
+    with open(file, 'r') as file:
+        content = file.read()
+        if search_term in content:
+            return True
         return False
 
 def set_hostname(hostname):
-    log(f"{Fore.LIGHTBLUE_EX}Setting hostname to {hostname}.")
+    log(f"SETTING HOSTNAME TO {hostname}..." , "YELLOW")
     run_command([f"hostnamectl set-hostname {hostname}"])
 
 def set_ip(ip):
-    log(f"{Fore.LIGHTBLUE_EX}Setting IP to {ip}.")
-    run_command([f"ip addr add {ip}/24 dev eth1"])
+    log(f"SETTING IP TO {ip}... " , "YELLOW")
+    log(f'Trying to run:ip addr add {ip}/24 dev eth1')
+    run_command([f'ip addr add {ip}/24 dev eth1'])
 
 def main():
     args = parse_arguments()
